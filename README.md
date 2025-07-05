c2d194df68f623b7317d17c6ca1505634d058813
nivedita <niveditasamarthh123@gmail.com>
2025-07-05 22:02:15 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Cat Mood Tracker
+
+Track my cat's daily moods
+
+#### License
+
+MIT
---
+++ moodpaw/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Cat Mood Tracker",
+			"type": "module",
+			"label": _("Cat Mood Tracker")
+		}
+	]
