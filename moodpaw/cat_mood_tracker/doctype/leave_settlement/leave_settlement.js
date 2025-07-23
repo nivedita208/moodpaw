cc878309666b6943643b644cd1b2bb65259b6b79
nivedita <niveditasamarthh123@gmail.com>
2025-07-23 16:05:19 +0530

    added custom button

--- moodpaw/cat_mood_tracker/doctype/leave_settlement/leave_settlement.js
+++ moodpaw/cat_mood_tracker/doctype/leave_settlement/leave_settlement.js
@@ -2,7 +2,59 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Leave Settlement", {
-    // after_save: function(frm) {
+    refresh:function(frm){
+        frm.add_custom_button(__("Create Payroll Entry"),function(){
+            // validation : only create payroll entry if ls is submitted
+            if (frm.doc.docstatus !== 1) {
+                frappe.msgprint("Please submit the Leave Settlement before creating a Payroll Entry.");
+                return;
+            }
+        // used for popup message
+           frappe.confirm("Do you want to create a new Payroll Entry?",
+            function(){
+                // Yes callback – you can call server method here
+                frappe.call({
+                    method: "moodpaw.cat_mood_tracker.doctype.leave_settlement.leave_settlement.create_payroll_entry" ,// path of python function (appname.path.to.function)
+                    args: { //It sends data (arguments) from the Leave Settlement form (frm) to the backend (Python method).
+                        // employee,from_date,to_date(these are key name used in args dict which are used in python code as arguments)
+                        employee: frm.doc.employee, 
+                        from_date:frm.doc.from_date, 
+                        to_date:frm.doc.to_date,
+                        leave_settlement: frm.doc.name 
+                    }, 
+                    callback: function(serverReply) {                       
+                        //frappe.msgprint("Payroll Entry Created:" + serverReply.message);
+                        if(serverReply.message.startsWith("Payroll Entry already exist")){
+                            frappe.msgprint(serverReply.message);
+                        }
+                        else{
+                            frappe.msgprint("Payroll Entry Created:" + serverReply.message);
+                            frm.reload_doc(); // to show the new linked entryrefresh to show the new linked payroll entry
+                        }
+                        
+                       
+                       console.log(serverReply);
+                       console.log(serverReply.message);
+                    }
+                })
+
+            },
+            function(){
+                // No callback – optional
+               frappe.msgprint("Oops not in the mood to create new payroll entry! no problem")
+            }
+
+           )
+
+        }
+        ,"", // No group
+         "btn-primary" //Blue color
+    )
+    }, 
+}
+);
+
+ // after_save: function(frm) {
     //     if (frm.doc.docstatus === 1) {
     //         // This only runs after submit
     //         frappe.set_route("Form", "Employee", frm.doc.employee);
@@ -15,5 +67,4 @@ frappe.ui.form.on("Leave Settlement", {
     //         frappe.set_route("Form", "Employee", frm.doc.employee);
     //     }
     // }
-});
 
--- moodpaw/cat_mood_tracker/doctype/leave_settlement/leave_settlement.py
+++ moodpaw/cat_mood_tracker/doctype/leave_settlement/leave_settlement.py
@@ -4,6 +4,8 @@ import frappe
 from frappe.model.document import Document
 from frappe.utils import nowdate
 
+
+
 class LeaveSettlement(Document):
 
     def before_submit(self):
@@ -60,6 +62,60 @@ class LeaveSettlement(Document):
                 """,
                 title="Status Updated"
     )
+
+#################################   custom button code ##############
+
+# whitelisted function must be outside the class
+@frappe.whitelist() # it is required to connect .py code with js(frappe.call) 
+def create_payroll_entry(employee, from_date, to_date, leave_settlement):
+    try:
+        # get leave settlement doc
+        existing = frappe.get_doc("Leave Settlement", leave_settlement)
+
+        #check if already exist
+        if existing.payroll_entry:
+            return f"Payroll Entry already exist: {existing.payroll_entry}"
+
+
+        # create new payroll entry document
+        pe = frappe.new_doc("Payroll Entry")
+        pe.company = frappe.defaults.get_user_default("Company")  # Default company
+        payable_account = frappe.db.get_value("Company", pe.company, "default_payroll_payable_account") #default payroll_payable_account
+        # Fetch default payroll payable account from that company
+        # Set payroll payable account
+        pe.payroll_payable_account = payable_account
+        pe.exchange_rate = 1.0
+        pe.payroll_frequency = "Monthly"
+        pe.start_date = from_date
+        pe.end_date = to_date
+        
+
+        # Add employee to child table
+        pe.append("employees",{ #here employess -- it’s the fieldname of the child table field inside Payroll Entry
+            "employee": employee #Put the value from the variable employee into the field named "employee" in the child table row.
+        }
+           )
+
+        pe.insert()
+
+        # link back to leave settlement if pe exist
+        existing.payroll_entry = pe.name
+        existing.save()
+
+        return pe.name
+        
+    except Exception as e:   
+        # Exception as e -->holds the actual error message 
+        frappe.throw(f" Something went wrong: {e}")
+       
+
+    # if i want to return name and status 
+    # return {
+#     "name": pe.name,
+#     "status": pe.status
+# }
+
+    
 
 
 
